a75ebaf97a422a6bba7abf1539ba89ebc50130b6
Tersawy <mohamedtersawy96@gmail.com>
2022-07-18 21:38:15 +0200

    Fix path

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ const express = require("express")
 
 const app = express()
 
-app.use(express.static("dist"));
+app.use(express.static("dist"))
 
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname,  "./dist/index.html"));
-});
+const path = require("path")
 
-let PORT = process.env.PORT || 3000;
+app.get("*", (req, res) => res.sendFile(path.join(__dirname,  "./dist/index.html")))
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
+let PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
