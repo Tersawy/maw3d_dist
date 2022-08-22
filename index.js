339f40177ebc91f73c8d95c79820b1b83876cb3b
Tersawy <mohamedtersawy96@gmail.com>
2022-08-22 23:09:53 +0200

    Change port

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ const path = require("path")
 
 app.get("*", (req, res) => res.sendFile(path.join(__dirname,  "./dist/index.html")))
 
-let PORT = process.env.PORT || 3000
+let PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
